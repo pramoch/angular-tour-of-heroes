ae4c18725bb01093a84c3621a0c189386b6b7f5d
Pramoch Chulapong <pramoch.chulapong@thomsonreuters.com>
2016-12-19 15:35:59 +0700

    Step6: Routing #1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,47 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
-  imports:      [ 
+  imports: [ 
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ 
+    AppComponent 
+  ]  
 })
 export class AppModule { }
